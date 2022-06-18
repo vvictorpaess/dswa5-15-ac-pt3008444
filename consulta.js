e9e1335c0f21e505763452a7026ff858037d875d
Victor Paes <victor.paes@aluno.ifsp.edu.br>
2022-06-17 23:43:08 -0300

    Ajustes

--- consulta.js
+++ consulta.js
@@ -4,7 +4,7 @@ const { MongoClient } = require('mongodb');
 
 // Connection URL
 const url =
-	'mongodb://dswa5:dswa5@cluster0-shard-00-00.ig8hl.mongodb.net:27017,cluster0-shard-00-01.ig8hl.mongodb.net:27017,cluster0-shard-00-02.ig8hl.mongodb.net:27017/?ssl=true&replicaSet=atlas-y7duym-shard-0&authSource=admin&retryWrites=true&w=majority';
+	'mongodb://dswa5:dswa5@cluster0-shard-00-00.ig8hl.mongodb.net:27017,cluster0-shard-00-01.ig8hl.mongodb.net:27017,cluster0-shard-00-02.ig8hl.mongodb.net:27017/ifsp?ssl=true&replicaSet=atlas-y7duym-shard-0&authSource=admin&retryWrites=true&w=majority';
 const client = new MongoClient(url);
 
 // Database Name
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = require("./config/express")(app);
 require("./config/passport")();
 
 const url =
-  "mongodb://dswa5:dswa5@cluster0-shard-00-00.ig8hl.mongodb.net:27017,cluster0-shard-00-01.ig8hl.mongodb.net:27017,cluster0-shard-00-02.ig8hl.mongodb.net:27017/?ssl=true&replicaSet=atlas-y7duym-shard-0&authSource=admin&retryWrites=true&w=majority";
+  "mongodb://dswa5:dswa5@cluster0-shard-00-00.ig8hl.mongodb.net:27017,cluster0-shard-00-01.ig8hl.mongodb.net:27017,cluster0-shard-00-02.ig8hl.mongodb.net:27017/ifsp?ssl=true&replicaSet=atlas-y7duym-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 require("./config/database.js")(url);
 
